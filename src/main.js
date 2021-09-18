60d47d8465f39b0a7e0b6172ea0b945c28246de5
delia_popescu <delia.popescu@principal33.com>
2021-09-18 23:12:03 +0300

    install axios dependencie

--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { faEdit } from '@fortawesome/free-solid-svg-icons';
 import { faTimes } from '@fortawesome/free-solid-svg-icons';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
 
 import App from './App.vue';
 import Header from './components/Header.vue';
@@ -12,6 +14,7 @@ import EmpoyeeTable from './components/Table.vue';
 library.add(faEdit, faTimes);
 
 const app = createApp(App);
+app.use(VueAxios, axios)
 
 app.component('font-awesome-icon',FontAwesomeIcon);
 app.component('cms-header', Header);
